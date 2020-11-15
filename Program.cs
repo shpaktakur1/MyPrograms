e5d4b78dc043da24691857c958e494bd1611e6af
shpaktakur1 <74150435+shpaktakur1@users.noreply.github.com>
2020-11-15 18:41:09 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,160 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq.Expressions;
-using System.Net.Http.Headers;
+using System.ComponentModel;
+using System.Numerics;
 using System.Text;
+using Z_HomeWork.TicTacToe;
 
-namespace E_Exceptions
+namespace Z_HomeWork
 {
-    //custiom exception type
-    public class CreditCardWithDrawException : Exception
-    {
-
-    }
-
-    class Program
-    {
+     class Program
+     {
+        private static TicTacToeGame g = new TicTacToeGame();
         static void Main(string[] args)
+       {
+            Console.WriteLine(GetPrintableState());
+
+            while(g.GetWinner() == Winner.GameIsUnFinished)
+            {
+                int index = int.Parse(Console.ReadLine());
+                g.MakeMove(index);
+
+                Console.WriteLine();
+                Console.WriteLine(GetPrintableState());
+            }
+
+            Console.WriteLine($"Result: {g.GetWinner()}");
+            Console.ReadLine();
+       }
+
+        static string GetPrintableState()
         {
-            // Файлы, директории, папки
-            try
+            var sb = new StringBuilder();
+            for(int i = 1; i<=7; i += 3)
             {
-                DireFileDemo();
+                sb.AppendLine("     |     |     |")
+                    .AppendLine(
+                    $"  {GetPrintableChar(i)}  |  {GetPrintableChar(i + 1)}  |  {GetPrintableChar(i + 2)}  |")
+                    .AppendLine("_____|_____|_____|");
+                   
             }
-            catch (Exception ex)
-            {
-
-            }
-
-
+            return sb.ToString();
+        }
+        static string GetPrintableChar(int index)
+        {
+            State state = g.GetState(index);
+            if (state == State.Unset)
+                return index.ToString();
+            return state == State.Cross ? "X" : "O";
         }
 
-        static void DireFileDemo()
+        
+        static void GuessNumberGame()
         {
-            //File.Copy("test.txt", @"D:\tmp\test_copy.txt", overwrite:true); // если true - файл будет перезатерт
-            File.Copy("test.txt", "test_copy.txt", overwrite: true);
-
-            File.Move("test_copy.txt", "test_copy_renamed.txt");
-
-            File.Delete("test_copy.txt");
-
-            if (File.Exists("test.txt")) // есть ли файл на диске
-            {
-                File.AppendAllText("test.txt", "bla");
-            }
-
-            File.Replace("test_2.txt", "test_3.txt", "test_backup.txt");
-
-            bool existsDir = Directory.Exists(@"D:\tmp");
-            if (existsDir)
-            {
-                var files = Directory.EnumerateFiles(@"D:\tmp", "*.txt", SearchOption.AllDirectories); // * - паттерн по которому ищет файлы
-                foreach (var file in files)
-                {
-                    Console.WriteLine(file);
-                }
-            }
-
-           //string fullPath = Path.Combine(@"D:\tmp", "\\bla", "file.txt");
-        }
-        static void FileDemo()
-        {
-            IEnumerable<string> lines = File.ReadLines("test.txt");
-
-            File.WriteAllText("test_2.txt", "My name is John");
-            File.WriteAllLines("test_3.txt", new string[] { "My name", "is John" });
-            File.WriteAllBytes("test_4.txt", new byte[] { 72, 101, 108, 108, 111 });
-
-            string alltext = File.ReadAllText("test_2.txt");
-            Console.WriteLine(alltext);
-
-            string[] allLines = File.ReadAllLines("test_3.txt");
-            Console.WriteLine(allLines[0]);
-            Console.WriteLine(allLines[1]);
-
-            byte[] bytes = File.ReadAllBytes("test_4.txt");
-            Console.WriteLine(Encoding.ASCII.GetString(bytes));
+            GuessNumberGame game = new GuessNumberGame(guessingPlayer: GuessingPlayer.Machine);
+            game.Start();
 
             Console.ReadLine();
-
-            //файлы и потоки
-            Stream fs = new FileStream("test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-
-
-            try
-            {
-                string str = "Hello, World";
-                byte[] strInBytes = Encoding.ASCII.GetBytes(str);
-
-                fs.Write(strInBytes, 0, strInBytes.Length);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.ToString()}");
-            }
-            finally
-            {
-                fs.Close();
-            }
-
-
-            Console.WriteLine("Now reading:");
-
-            using (Stream readingStream = new FileStream("test.txt", FileMode.Open, FileAccess.Read)) // создаем объект который в конце должен быть закрыт
-            {
-                byte[] temp = new byte[readingStream.Length];
-
-                int bytesToRead = (int)readingStream.Length;
-                int bytesRead = 0;
-                while (bytesRead > 0)
-                {
-                    int n = readingStream.Read(temp, bytesRead, bytesToRead); // bytesRead - количество уже прочитанных файлов, bytesToRead - количество оставшихся
-
-                    if (n == 0)
-                        break;
-
-                    bytesRead += n;
-                    bytesToRead -= n;
-
-                }
-                string str = Encoding.ASCII.GetString(temp, 0, temp.Length);
-
-                Console.WriteLine(str);
-
-                Console.ReadLine();
-            }
-
         }
-        static void ExceptionsDemo()
+        static void ComplexNumbers()
         {
-            //обработка исключений(механизм, c помощью которого обрабатываем ошибки)
-            Console.WriteLine("Please input a number");
+            //Complex c1 = new Complex(1, 1);
+            //Complex c2 = new Complex(1, 2);
 
-            string result = Console.ReadLine();
-
-            int number = 0;
-            try
-            {
-                number = int.Parse(result);
-            }
-            catch (OverflowException ex)
-            {
-
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine("A format exception has occured");
-                Console.WriteLine("Information is below:");
-                Console.WriteLine(ex.ToString());
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-            Console.WriteLine(number);
+            //Complex result = c1.Plus(c2);
+            //Console.WriteLine($"result. Real ={result.Real}; Imaginary={result.Imaginary}");
         }
-    }
-
-}        
+     }
+}
